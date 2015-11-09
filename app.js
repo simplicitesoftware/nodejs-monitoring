df68622c5ab9f88f0bb327cabd7bec9c1dbd6e93
David AZOULAY <dazoulay@simplicite.fr>
2015-11-09 13:10:50 +0100

    Added force mode

--- app.js
+++ app.js
@@ -48,38 +48,42 @@ conn.connect(function(err) {
 				}
 			}
 
-			function monitorApps() {
-				for (var name in apps) {
-					var session = apps[name].session;
-					if (session) {
-						console.log('Request to ' + name);
-						session.getHealth().then(function(health) {
-							try {
-								var n = health._scope._name;
-								delete health._scope; // Remove scope reference from response
-								health.name = n; // Add name for health view
-								apps[n].status = 200;
-								apps[n].health = health;
-								// TODO: save health data
-								console.log('Response from ' + n + ' = ' + health.platform.status);
-							} catch(e) {
-								console.error(e);
-							}
-						}, function(err) {
-							try {
-								var n = err._scope._name;
-								delete err._scope; // Remove scope reference from response
-								apps[n].status = err.status || 404;
-								delete apps[n].health;
-								console.error('Error on ' + n + ' = ' + err.message);
-							} catch(e) {
-								console.error(e);
-							}
-						});
-					}
+			function monitorApp(name, callback) {
+				var session = apps[name].session;
+				if (session) {
+					console.log('Request to ' + name);
+					session.getHealth().then(function(health) {
+						try {
+							var n = health._scope._name;
+							delete health._scope; // Remove scope reference from response
+							health.name = n; // Add name for health view
+							apps[n].status = 200;
+							apps[n].health = health;
+							// TODO: save health data
+							console.log('Response from ' + n + ' = ' + health.platform.status);
+							if (callback) callback.call(this, name);
+						} catch(e) {
+							console.error(e);
+						}
+					}, function(err) {
+						try {
+							var n = err._scope._name;
+							delete err._scope; // Remove scope reference from response
+							apps[n].status = err.status || 404;
+							delete apps[n].health;
+							console.error('Error on ' + n + ' = ' + err.message);
+						} catch(e) {
+							console.error(e);
+						}
+					});
 				}
+			}
+
+			function monitorApps() {
+				for (var name in apps) monitorApp(name);
 				setTimeout(monitorApps, interval * 1000);
 			}
+			
 			monitorApps();
 			
 			var express = require('express');
@@ -103,12 +107,19 @@ conn.connect(function(err) {
 				var name = req.query ? req.query.name : null;
 				if (name) {
 					console.log('Health page requested for name = ' + name);
-					var h = apps[name].health;
-					
-					if (h)
-						res.render('health', h);
+					function render(n) {
+						console.log(n);
+						var h = apps[n].health;
+						if (h)
+							res.render('health', h);
+						else
+							res.render('error', { error: 'No data for name = ' + n });
+					}
+					var force = req.query ? req.query.force : null;
+					if (force)
+						monitorApp(name, render);
 					else
-						res.render('error', { error: 'No data' });
+						render(name);
 				} else
 					res.render('error', { error: 'Missing name' });
 			});
