ca714f888154f8615a495046a4679c84542f318e
David AZOULAY <dazoulay@simplicite.fr>
2015-11-09 18:04:44 +0100

    Strict mode

--- app.js
+++ app.js
@@ -12,12 +12,6 @@ var conn = mysql.createConnection({
 });
 
 conn.connect(function(err) {
-	if (err) {
-		console.error('MySQL error: ' + err.stack);
-		return;
-	} else
-		console.log('Connected to MySQL');
-
 	function getApps(callback) {
 		conn.query('select name, url, active from monitoring_app order by name', callback);
 	}
@@ -35,9 +29,52 @@ conn.connect(function(err) {
 		conn.query('insert into monitoring_data(name, data) values (?)', { name: name, data: JSON.stringify(data) }, callback);
 	}
 
+	if (err) {
+		console.error('MySQL error: ' + err.stack);
+		return;
+	} else
+		console.log('Connected to MySQL');
+
 	var simplicite = require('simplicite');
 	var apps;
 	getApps(function (err, rs) {
+
+		function monitorApp(name, callback) {
+			var session = apps[name].session;
+			if (session) {
+				console.log('Request to ' + name);
+				session.getHealth().then(function(health) {
+					try {
+						var n = health._scope._name;
+						delete health._scope; // Remove scope reference from response
+						health.name = n; // Add name for health view
+						apps[n].status = 200;
+						apps[n].health = health;
+						// TODO: save health data
+						console.log('Response from ' + n + ' = ' + health.platform.status);
+						if (callback) callback.call(this, name);
+					} catch(e) {
+						console.error(e);
+					}
+				}, function(err) {
+					try {
+						var n = err._scope._name;
+						delete err._scope; // Remove scope reference from response
+						apps[n].status = err.status || 404;
+						delete apps[n].health;
+						console.error('Error on ' + n + ' = ' + err.message);
+					} catch(e) {
+						console.error(e);
+					}
+				});
+			}
+		}
+
+		function monitorApps() {
+			for (var name in apps) monitorApp(name);
+			setTimeout(monitorApps, interval * 1000);
+		}
+		
 		apps = {};
 		if (!err) {
 			for (var k = 0; k < rs.length; k++) {
@@ -49,43 +86,6 @@ conn.connect(function(err) {
 					apps[r.name].session._name = r.name; // ZZZ Name stored at session level to retreive it then() functions
 				}
 			}
-
-			function monitorApp(name, callback) {
-				var session = apps[name].session;
-				if (session) {
-					console.log('Request to ' + name);
-					session.getHealth().then(function(health) {
-						try {
-							var n = health._scope._name;
-							delete health._scope; // Remove scope reference from response
-							health.name = n; // Add name for health view
-							apps[n].status = 200;
-							apps[n].health = health;
-							// TODO: save health data
-							console.log('Response from ' + n + ' = ' + health.platform.status);
-							if (callback) callback.call(this, name);
-						} catch(e) {
-							console.error(e);
-						}
-					}, function(err) {
-						try {
-							var n = err._scope._name;
-							delete err._scope; // Remove scope reference from response
-							apps[n].status = err.status || 404;
-							delete apps[n].health;
-							console.error('Error on ' + n + ' = ' + err.message);
-						} catch(e) {
-							console.error(e);
-						}
-					});
-				}
-			}
-
-			function monitorApps() {
-				for (var name in apps) monitorApp(name);
-				setTimeout(monitorApps, interval * 1000);
-			}
-			
 			monitorApps();
 			
 			var express = require('express');
@@ -95,6 +95,15 @@ conn.connect(function(err) {
 			server.set('views', __dirname + '/views');
 
 			server.get('/', function(req, res) {
+				function render(n) {
+					console.log(n);
+					var h = apps[n].health;
+					if (h)
+						res.render('health', h);
+					else
+						res.render('error', { error: 'No data for name = ' + n });
+				}
+
 				res.header('Cache-Control', 'private, no-cache, no-store, no-transform, must-revalidate');
 				res.header('Expires', '-1');
 				res.header('Pragma', 'no-cache');
@@ -102,14 +111,6 @@ conn.connect(function(err) {
 				var name = req.query ? req.query.name : null;
 				if (name) {
 					console.log('Health page requested for name = ' + name);
-					function render(n) {
-						console.log(n);
-						var h = apps[n].health;
-						if (h)
-							res.render('health', h);
-						else
-							res.render('error', { error: 'No data for name = ' + n });
-					}
 					var force = req.query ? req.query.force : null;
 					if (force)
 						monitorApp(name, render);
